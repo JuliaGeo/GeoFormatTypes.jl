f9e9d385484b40073f6c6b3608caf1d13f6f7fc1
Rafael Schouten <rafaelschouten@gmail.com>
2019-11-12 13:29:50 +1100

    add more types for ArchGDAL use

--- src/GeoFormatTypes.jl
+++ src/GeoFormatTypes.jl
@@ -7,27 +7,31 @@ module GeoFormatTypes
     read(path, String)
 end GeoFormatTypes
 
-export WellKnownText, WellKnownBinary, Proj4String, 
+export WellKnownText, WellKnownBinary, ProjString, 
        EPSGcode, GML, KML, GeoJSON, GeoJSONdict
 
 const PROJ_PREFIX = "+proj="
 const EPSG_PREFIX = "EPSG:"
 
-val(x::SpatialType) = x.val
-
 """
-Proj4 string
+Proj string
 """
-struct Proj4String
+struct ProjString
     val::String
-end
-Proj4String(input::AbstractString) = begin
-    startswith(input, PROJ_PREFIX) || 
-        throw(ArgumentError("String $input does not start with $PROJ_PREFIX"))
-    Proj4String(input)
+    ProjString(input::String) = begin
+        startswith(input, PROJ_PREFIX) || 
+            throw(ArgumentError("String $input does not start with $PROJ_PREFIX"))
+        new(input)
+    end
 end
 
-Base.show(io::IO, crs::Proj4String) = print(io, "Proj4:\n$(val(x))")
+val(x::ProjString) = x.val
+
+Base.show(io::IO, x::ProjString) = print(io, "Proj:\n$(val(x))")
+
+struct NetCDF_CRS
+    val::Dict{String,Any}
+end
 
 """
 Well known text
@@ -35,6 +39,8 @@ Well known text
 struct WellKnownText
     val::String
 end
+
+val(x::WellKnownText) = x.val
 
 Base.show(io::IO, x::WellKnownText) = print(io, "Well Known Text:\n$(val(x))")
 
@@ -44,6 +50,8 @@ Well known binary
 struct WellKnownBinary
     val::String
 end
+
+val(x::WellKnownBinary) = x.val
 
 Base.show(io::IO, x::WellKnownBinary) = print(io, "Well Known Binary")
 
@@ -57,12 +65,25 @@ end
 Constructor for EPSG:1234 style strings
 """
 EPSGcode(input::AbstractString) = begin
-    startswith(input, EPSG_PREFIX) || throw(ArgumentError("String $epsg_string does no start with $EPSG_PREFIX"))
+    startswith(input, EPSG_PREFIX) || throw(ArgumentError("String $input does no start with $EPSG_PREFIX"))
     code = parse(Int, input[findlast(EPSG_PREFIX, input).stop+1:end])
     EPSGcode(code)
 end
 
+val(x::EPSGcode) = x.val
+
 Base.show(io::IO, x::EPSGcode) = print(io, "EPSGcode: $EPSG_PREFIX$(val(x))")
+
+"""
+ESRI code
+"""
+struct ESRI
+    val::String
+end
+
+val(x::ESRI) = x.val
+
+Base.show(io::IO, x::ESRI) = print(io, "ESRI: $(val(x))")
 
 """
 Keyhole Markup Language
@@ -70,6 +91,8 @@ Keyhole Markup Language
 struct KML
     val::String
 end
+
+val(x::KML) = x.val
 
 Base.show(io::IO, x::KML) = print(io, "KML:\n$(val(x))")
 
@@ -80,6 +103,8 @@ struct GML
     val::String
 end
 
+val(x::GML) = x.val
+
 Base.show(io::IO, x::GML) = print(io, "GML:\n$(val(x))")
 
 """
@@ -89,24 +114,30 @@ struct GeoJSON
     val::String
 end
 
+val(x::GeoJSON) = x.val
+
 Base.show(io::IO, x::GeoJSON) = print(io, "GeoJSON:\n$(repr(val(x)))")
 
 """
 GeoJSON 
 """
-struct GeoJSONdict <: AbstractDict
-    val::Dict{String,Any}
+struct GeoJSONdict{String,V} <: AbstractDict{String,V}
+    val::Dict{String,V}
 end
 
-Base.show(io::IO, x::GeoJSONdict) = print(io, "GeoJSON Dict:\n$(repr(val(x)))")
+val(x::GeoJSONdict) = x.val
 
-values(iterator::GeoJSONdict) = values(val(iterator))
-get(collection::GeoJSONdict, key, default)
-get(f::Base.Callable, collection::GeoJSONdict, key)
+Base.show(io::IO, x::GeoJSONdict) = print(io, "GeoJSON Dict:\n$(val(x))")
+
+getindex(collection::GeoJSONdict, key) = getindex(val(collection), key)
+setindex!(collection::GeoJSONdict, x, key) = setindex!(val(collection), x, key)
+get(collection::GeoJSONdict, key, default) = get(val(collection), key, default)
+get(f::Base.Callable, collection::GeoJSONdict, key) = get(f, val(collection), key)
 get!(f::Base.Callable, collection::GeoJSONdict, key) = get!(f, val(collection), key)
-values(d::GeoJSONdict) = values(val(collection))
-getkey(collection::GeoJSONdict, key, default) =  getkey(val(collection), key, default)
-delete!(collection::GeoJSONdict, key) =  delete!(val(collection), key)
-pop!(collection::GeoJSONdict, key[, default]) = pop!(collection, key[, default])
+keys(collection::GeoJSONdict) = keys(val(collection))
+values(collection::GeoJSONdict) = values(val(collection))
+getkey(collection::GeoJSONdict, key, default) = getkey(val(collection), key, default)
+delete!(collection::GeoJSONdict, key) = delete!(val(collection), key)
+pop!(collection::GeoJSONdict, key, args...) = pop!(collection, key, args...)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using GeoFormatTypes, Test
 
-@testset "GeoFormatTypes.jl" begin
+WellKnownText("")
+# WellKnownBinary
+ProjString("+proj=merc +lat_ts=56.5 +ellps=GRS80")
+@test_throws ArgumentError ProjString("+lat_ts=56.5 +ellps=GRS80")
 
-end
+@test EPSGcode(4326) == EPSGcode("EPSG:4326")
+@test_throws ArgumentError EPSGcode("TEST:4326")
+
+GML("")
+KML("")
+GeoJSON("")
+GeoJSONdict(Dict());

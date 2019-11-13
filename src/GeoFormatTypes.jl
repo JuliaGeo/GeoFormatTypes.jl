b2a7ae362ab0d39e07493197f08e5b925c0341b1
Rafael Schouten <rafaelschouten@gmail.com>
2019-11-13 14:12:57 +1100

    refactor

--- src/GeoFormatTypes.jl
+++ src/GeoFormatTypes.jl
@@ -1,64 +1,66 @@
 module GeoFormatTypes
 
 # Use the README as the module docs
-@doc let 
+@doc let
     path = joinpath(dirname(@__DIR__), "README.md")
     include_dependency(path)
     read(path, String)
 end GeoFormatTypes
 
-export WellKnownText, WellKnownBinary, ProjString, 
-       EPSGcode, GML, KML, GeoJSON, GeoJSONdict
+export GeoFormat, EPSGcode, ESRI, GML, GeoJSON, KML,
+       ProjString, WellKnownText, WellKnownBinary
 
 const PROJ_PREFIX = "+proj="
 const EPSG_PREFIX = "EPSG:"
 
 """
+Abstract supertype for all formats
+"""
+abstract type GeoFormat end
+
+val(x::GeoFormat) = x.val
+
+# Most GeoFormat types wrap String or have a constructor for string inputs
+Base.convert(::Type{String}, input::GeoFormat) = val(input)
+Base.convert(::Type{T}, input::AbstractString) where T <: GeoFormat = T(input)
+
+"""
 Proj string
 """
-struct ProjString
+struct ProjString <: GeoFormat
     val::String
     ProjString(input::String) = begin
-        startswith(input, PROJ_PREFIX) || 
+        startswith(input, PROJ_PREFIX) ||
             throw(ArgumentError("String $input does not start with $PROJ_PREFIX"))
         new(input)
     end
 end
 
-val(x::ProjString) = x.val
-
 Base.show(io::IO, x::ProjString) = print(io, "Proj:\n$(val(x))")
-
-struct NetCDF_CRS
-    val::Dict{String,Any}
-end
 
 """
 Well known text
 """
-struct WellKnownText
+struct WellKnownText <: GeoFormat
     val::String
 end
 
-val(x::WellKnownText) = x.val
-
 Base.show(io::IO, x::WellKnownText) = print(io, "Well Known Text:\n$(val(x))")
+
 
 """
 Well known binary
 """
-struct WellKnownBinary
-    val::String
+struct WellKnownBinary{T} <: GeoFormat
+    val::T
 end
-
-val(x::WellKnownBinary) = x.val
 
 Base.show(io::IO, x::WellKnownBinary) = print(io, "Well Known Binary")
 
 """
 EPSG code
 """
-struct EPSGcode
+struct EPSGcode <: GeoFormat
     val::Int
 end
 """
@@ -70,74 +72,48 @@ EPSGcode(input::AbstractString) = begin
     EPSGcode(code)
 end
 
-val(x::EPSGcode) = x.val
+Base.convert(::Type{Int}, input::EPSGcode) = val(input)
+Base.convert(::Type{String}, input::EPSGcode) = string(EPSG_PREFIX, val(input))
+Base.convert(::Type{EPSGcode}, input::Int) = EPSGcode(input)
 
 Base.show(io::IO, x::EPSGcode) = print(io, "EPSGcode: $EPSG_PREFIX$(val(x))")
 
 """
 ESRI code
 """
-struct ESRI
+struct ESRI <: GeoFormat
     val::String
 end
-
-val(x::ESRI) = x.val
 
 Base.show(io::IO, x::ESRI) = print(io, "ESRI: $(val(x))")
 
 """
 Keyhole Markup Language
 """
-struct KML
+struct KML <: GeoFormat
     val::String
 end
-
-val(x::KML) = x.val
 
 Base.show(io::IO, x::KML) = print(io, "KML:\n$(val(x))")
 
 """
-Geography MarkupLanguage
+Geography Markup Language
 """
-struct GML
+struct GML <: GeoFormat
     val::String
 end
-
-val(x::GML) = x.val
 
 Base.show(io::IO, x::GML) = print(io, "GML:\n$(val(x))")
 
 """
-GeoJSON 
-"""
-struct GeoJSON
-    val::String
-end
+GeoJSON
 
-val(x::GeoJSON) = x.val
+Can be a string or a Dict? Will need to handle convert for Dict.
+"""
+struct GeoJSON{T} <: GeoFormat
+    val::T
+end
 
 Base.show(io::IO, x::GeoJSON) = print(io, "GeoJSON:\n$(repr(val(x)))")
-
-"""
-GeoJSON 
-"""
-struct GeoJSONdict{String,V} <: AbstractDict{String,V}
-    val::Dict{String,V}
-end
-
-val(x::GeoJSONdict) = x.val
-
-Base.show(io::IO, x::GeoJSONdict) = print(io, "GeoJSON Dict:\n$(val(x))")
-
-getindex(collection::GeoJSONdict, key) = getindex(val(collection), key)
-setindex!(collection::GeoJSONdict, x, key) = setindex!(val(collection), x, key)
-get(collection::GeoJSONdict, key, default) = get(val(collection), key, default)
-get(f::Base.Callable, collection::GeoJSONdict, key) = get(f, val(collection), key)
-get!(f::Base.Callable, collection::GeoJSONdict, key) = get!(f, val(collection), key)
-keys(collection::GeoJSONdict) = keys(val(collection))
-values(collection::GeoJSONdict) = values(val(collection))
-getkey(collection::GeoJSONdict, key, default) = getkey(val(collection), key, default)
-delete!(collection::GeoJSONdict, key) = delete!(val(collection), key)
-pop!(collection::GeoJSONdict, key, args...) = pop!(collection, key, args...)
 
 end # module
